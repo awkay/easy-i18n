7f1d40047d3ebbe081c4dc61e6c8523d49dcc8e0
Tony Kay <tony@teamunify.com>
2014-02-03 10:52:00 -0800

    added properties file for sample ant, and added sample code with translations

---
+++ src/test/resources/ExtractMe.java
@@ -0,0 +1,10 @@
+/**
+ * An artifact for doing tests on the Ant scripts for message extraction.
+ */
+public class ExtractMe
+{
+  public void someMethod() {
+    I.tr("Add");
+    I.tr("Remove");
+  }
+}
